2118f206bbdb7ee2b7a0ef3404d264f3185988fb
Roberto Zappa <roberto.zappa01@universitadipavia.it>
2023-03-11 21:05:48 +0100

    version 0.2, first version with "weight and bias"

--- Lab1-CNNs/Exercise 1.1 A baseline MLP/train.py
+++ Lab1-CNNs/Exercise 1.1 A baseline MLP/train.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from tqdm import tqdm
 from model import MultilayerPerceptron
 from utils import (get_loaders, save_checkpoint, load_checkpoint, metrics, eval_fn, save_plot, Lion)
+import wandb
 
 # Hyperparameters and other settings
 LEARNING_RATE = 1e-3
@@ -16,6 +17,8 @@ NUM_WORKERS = 8
 PIN_MEMORY = True
 LOAD_MODEL = False
 WEIGHT_DIR = "result/checkpoint.pth.tar"
+
+
 # TODO: add Weight and bias
 
 
@@ -23,7 +26,7 @@ def train_fn(epoch, loader, model, optimizer, scheduler, loss_fn, scaler, metric
     model.train()
     running_loss = 0
 
-    for (data, target) in tqdm(loader, desc=f"Epoch {epoch + 1 }"):
+    for (data, target) in tqdm(loader, desc=f"Epoch {epoch + 1}"):
         data = data.to(DEVICE, non_blocking=True)
         target = target.to(DEVICE, non_blocking=True)
 
@@ -50,7 +53,22 @@ def train_fn(epoch, loader, model, optimizer, scheduler, loss_fn, scaler, metric
 
 
 def main():
-    model = MultilayerPerceptron(in_channel=28*28, num_layer=2, n_class=10).to(DEVICE)
+    prova = wandb.init(
+        # set the wandb project where this run will be logged
+        project="MultilayerPerceptron",
+        group='Multi Layer Perceptron',
+        tags=["baseline"],
+        name='0',
+
+        # track hyperparameters and run metadata
+        config={
+            "learning_rate": 1e-3,
+            "architecture": "Multi Layer Perceptron",
+            "dataset": "Mnist",
+            "epochs": 5,
+        }
+    )
+    model = MultilayerPerceptron(in_channel=28 * 28, num_layer=2, n_class=10).to(DEVICE)
     if LOAD_MODEL:
         load_checkpoint(torch.load(WEIGHT_DIR), model)
 
@@ -63,18 +81,24 @@ def main():
                                                     steps_per_epoch=len(train_loader),
                                                     epochs=NUM_EPOCHS)
     metric_collection = metrics(DEVICE)
+    wandb.watch(model, criterion=loss_fn, log="all", log_freq=2, log_graph=True)
 
     train_l, train_d, test_l, test_d = [], [], [], []
     patience = PATIENCE
     max_accuracy = 0
 
     for epoch in range(NUM_EPOCHS):
-        train_loss, train_dice = train_fn(epoch, train_loader, model, optimizer, scheduler, loss_fn, scaler,
-                                          metric_collection)
+        train_loss, train_accuracy = train_fn(epoch, train_loader, model, optimizer, scheduler, loss_fn, scaler,
+                                              metric_collection)
         # check accuracy
         test_loss, test_accuracy = eval_fn(test_loader, model, loss_fn, metric_collection, DEVICE)
+        wandb.log({"train_loss": train_loss,
+                   "train_accuracy": train_accuracy,
+                   'test_loss': test_loss,
+                   'test_accuracy': test_accuracy,
+                   })
         train_l.append(train_loss)
-        train_d.append(train_dice)
+        train_d.append(train_accuracy)
         test_l.append(test_loss)
         test_d.append(test_accuracy)
         # save model
@@ -89,7 +113,9 @@ def main():
         if patience == 0:
             break
         patience -= 1
+
     save_plot(train_l, train_d, test_l, test_d)
+    wandb.finish()
     sys.exit()
 
 
