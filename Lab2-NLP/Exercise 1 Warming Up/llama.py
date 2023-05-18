963f82034efbc1aa33d89d0d63d432a2772dc0b5
robertozappa <roberto.zappa01@universitadipavia.it>
2023-05-18 18:24:44 +0200

    version 0.1

--- Lab2-NLP/Exercise 1 Warming Up/llama.py
+++ Lab2-NLP/Exercise 1 Warming Up/llama.py
@@ -3,8 +3,9 @@ import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, AutoTokenizer, AutoModelForCausalLM
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
+from torch.nn import Transformer as T
 import math
+from einops import rearrange, repeat
 
 """
 time to build a llama like Transformer model:
@@ -17,13 +18,14 @@ time to build a llama like Transformer model:
 
 
 class Embedding(nn.Module):
-    def __init__(self, vocab_size, embedding_size):
+    def __init__(self, vocab_size, embedding_size, max_len=4096):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_size)
         self.tokenizer = AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Instruct-3B-v1")
+        self.max_len = max_len
 
     def forward(self, string):
-        inputs = self.tokenizer(string, return_tensors="pt", max_length=4096)
+        inputs = self.tokenizer(string, return_tensors="pt", max_length=self.max_len, truncation=True)
         token_id = inputs.input_ids
         # attention_mask = inputs.attention_mask
         return self.embedding(token_id)
@@ -43,52 +45,67 @@ class RMSNorm(torch.nn.Module):
         return output * self.weight
 
 
-def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
-    ndim = x.ndim
-    assert 0 <= 1 < ndim
-    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
-    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
-    return freqs_cis.view(*shape)
+def freq_pos_enc(dim, max_seq_len):
+    # inverse frequencies [theta_1, theta_2, ..., theta_dim/2]
+    inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2, dtype=torch.float) / dim))  # (dim/2,)
+    # positions up to max_seq_len
+    pos = torch.arange(max_seq_len)  # (max_seq_len,)
+    # frequency position encoding (outer product: pos x inv_freq)
+    pos_enc = torch.einsum("p, f -> p f", pos, inv_freq)  # (max_seq_len, dim/2)
+    # duplicate each element along inverse frequency dimension
+    return repeat(pos_enc, "... f -> ... (f r)", r=2)  # (max_seq_len, dim)
 
 
-def apply_rotary_emb(xq: torch.Tensor, xk: torch.Tensor, freqs_cis: torch.Tensor, ) -> Tuple[
-    torch.Tensor, torch.Tensor]:
-    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
-    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
-    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
-    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
-    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
-    return xq_out.type_as(xq), xk_out.type_as(xk)
+def rotate_half(u):
+    # last dimension of u is [u1, u2, u3, u4, ...]
+    u = rearrange(u, '... (d r) -> ... d r', r=2)
+    u1, u2 = u.unbind(dim=-1)
+    u = torch.stack((-u2, u1), dim=-1)
+    # last dimension of result is [-u2, u1, -u4, u3, ...]
+    return rearrange(u, '... d r -> ... (d r)')
+
+
+def rotate(u, pos_enc):
+    num_tokens = u.shape[-2]
+    pos_enc = pos_enc[:num_tokens]
+    return u * pos_enc.cos() + (rotate_half(u) * pos_enc.sin())
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, embed_dim, num_heads=8):
+    def __init__(self, embed_dim, num_heads=8, max_seq_len=4096):
         super().__init__()
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.d_k = embed_dim // num_heads
+
+        self.freq_pos_enc = freq_pos_enc(self.d_k, max_seq_len)
+
+        self.k_cache = None
+        self.v_cache = None
+
         self.wq = nn.Linear(embed_dim, self.d_k * num_heads)
         self.wk = nn.Linear(embed_dim, self.d_k * num_heads)
         self.wv = nn.Linear(embed_dim, self.d_k * num_heads)
         self.wo = nn.Linear(self.d_k * num_heads, embed_dim)
 
-    def forward(self, x, mask=None):
+    def forward(self, x):
         bsz, seq_len, _ = x.shape  # bsz, seq_len, embed_dim
         q, k, v = self.wq(x), self.wk(x), self.wv(x)  # bsz, seq_len, d_k * num_heads [1, 2, 4096]
-        q = q.view(bsz, seq_len, self.num_heads, self.d_k)  # [1, 2, 8, 512]
-        k = k.view(bsz, seq_len, self.num_heads, self.d_k)
-        v = v.view(bsz, seq_len, self.num_heads, self.d_k)
+        q = q.view(bsz,  seq_len, self.num_heads, self.d_k).transpose(1, 2)  # [1, 8, 2, 512]
+        k = k.view(bsz,  seq_len, self.num_heads, self.d_k).transpose(1, 2)
+        v = v.view(bsz,  seq_len, self.num_heads, self.d_k).transpose(1, 2)
 
-        # TODO: implement rotary embeddings
+        q, k = rotate(q, self.freq_pos_enc), rotate(k, self.freq_pos_enc)
 
-        q = q.transpose(1, 2)  # [1, 8, 2, 512]
-        k = k.transpose(1, 2)
-        v = v.transpose(1, 2)
+        if self.k_cache:
+            old_k, old_v = self.k_cache, self.v_cache
+            k = torch.cat(old_k, k)
+            v = torch.cat(old_v, v)
+        else:
+            self.k_cache, self.v_cache = k, v
 
-        attention_score = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(self.d_k)  # [1, 8, 2, 2]
-
-        if mask is not None:
-            attention_score = attention_score + mask
+        mask = T.generate_square_subsequent_mask(k.shape[2])
+        attention_score = (torch.matmul(q, k.transpose(2, 3)) / math.sqrt(self.d_k)) + mask  # [1, 8, 2, 2]
 
         attention_weights = F.softmax(attention_score, dim=-1)
         attention = torch.matmul(attention_weights, v).transpose(1, 2).contiguous().view(bsz, seq_len, -1) #[1, 2, 4096]
@@ -124,10 +141,11 @@ class TransformerBlock(nn.Module):
 
 
 class LittleLLama(nn.Module):
-    def __init__(self, vocab_size, embedding_size, num_heads=8, num_layers=6):
+    def __init__(self, vocab_size, embedding_size, num_heads=8, num_layers=6, max_seq_len=1024):
         super().__init__()
-        self.embedding = Embedding(vocab_size, embedding_size)
-        self.attention = MultiHeadAttention(embedding_size, num_heads)
+        self.embedding = Embedding(vocab_size, embedding_size, max_seq_len)
+        self.attention = MultiHeadAttention(embedding_size, num_heads, max_seq_len)
+
         # self.transformer = nn.Sequential(*[TransformerBlock(embedding_size, num_heads) for _ in range(num_layers)])
         # self.linear = nn.Linear(embedding_size, vocab_size)
 
